e7f319bb1c57ee4b0fd9ec4abba4996f857d67c2
Amrutha Varshini Yenugonda <167894265+AmruthaVarshini111@users.noreply.github.com>
2026-05-16 01:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from PIL import Image
+import cv2
+import numpy as np
 
 st.title("DeepFake Detection using Explainable AI")
 
@@ -8,8 +10,12 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
 
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    st.success("Prediction: FAKE")
+    img_array = np.array(image)
 
-    st.write("Grad-CAM explanation generated.")
+    gray = cv2.cvtColor(img_array, cv2.COLOR_RGB2GRAY)
+
+    st.image(gray, caption="Processed Output", use_container_width=True)
+
+    st.success("Prediction: Fake Image Detected")
