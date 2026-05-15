c169e350abb33887435bbd7c2f14f85db9368863
Amrutha Varshini Yenugonda <167894265+AmruthaVarshini111@users.noreply.github.com>
2026-05-16 01:44:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="DeepFake Detector")
-
 st.title("DeepFake Detection using Explainable AI")
 
-uploaded_file = st.file_uploader(
-    "Upload an image",
-    type=["jpg", "jpeg", "png"]
-)
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image")
-
-    st.success("Prediction Complete")
-    st.write("Result: Fake Image Detected")
+    st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
+    st.success("Image uploaded successfully!")
