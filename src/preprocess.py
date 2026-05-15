461e789ff972a243a79f3cd8f8d07d4dfd716d15
Amrutha Varshini Yenugonda <167894265+AmruthaVarshini111@users.noreply.github.com>
2026-05-15 23:29:05 +0530

    Add image preprocessing module

---
+++ src/preprocess.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+def preprocess_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.resize(image, (224, 224))
+    image = image / 255.0
+    return image
+
+print("Preprocessing module ready")
